bdcc651832648ce637d2536a2aaa9c6f1c2fee0e
deric254 <dericmarangu@gmail.com>
2026-08-11 19:26:43 +0300

    111
    1

--- frontend/src/api/config.ts
+++
@@ -1,7 +0,0 @@
-import { api } from './client'
-import type { BusinessConfigOut, BusinessConfigUpdate } from '../types/config'
-
-export const configApi = {
-  get: (timeoutMs?: number) => api.get<BusinessConfigOut>('/config', undefined, timeoutMs),
-  update: (payload: BusinessConfigUpdate) => api.patch<BusinessConfigOut>('/config', payload),
-}
--- frontend/src/api/setup.ts
+++
@@ -1,9 +0,0 @@
-import { api, uploadFile } from './client'
-import type { SetupStatusOut, FirstUserCreate, RestoreResult } from '../types/api'
-
-export const setupApi = {
-  status: (timeoutMs?: number) => api.get<SetupStatusOut>('/setup/status', undefined, timeoutMs),
-  createFirstUser: (payload: FirstUserCreate) => api.post<void>('/setup/first-user', payload),
-  restoreFromFile: (file: File, passphrase: string) =>
-    uploadFile<RestoreResult>('/setup/restore-from-file', file, { passphrase }),
-}
